71090c43e3da363a2814f56c61810ddfccb32076
MateoSaettone <mattsmadzyt@gmail.com>
2024-09-13 16:46:11 -0400

    added backend fastapi capabilities

---
+++ FastAPI/main.py
@@ -0,0 +1,75 @@
+from fastapi import FastAPI, HTTPException, Depends
+from typing import Annotated, List
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from database import SessionLocal, engine
+import models
+from fastapi.middleware.cors import CORSMiddleware
+from database import engine
+
+# .\env\Scripts\Activate
+# uvicorn main:app --reload
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+)
+
+
+class TransactionBase(BaseModel):
+    amount: float
+    category: str
+    description: str
+    is_income: bool
+    date: str
+
+class TransactionModel(TransactionBase):
+    id: int
+    
+    class Config:
+        orm_mode = True
+    
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+models.Base.metadata.create_all(bind=engine)
+
+
+@app.post("/transactions/", response_model=TransactionModel)
+
+async def create_transaction(transaction: TransactionBase, db: db_dependency):
+    print('he')
+    db_transaction = models.Transaction(**transaction.model_dump())
+    db.add(db_transaction)
+    db.commit()
+    db.refresh(db_transaction)
+    return db_transaction
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- React/src/navigation/Sidenav.js
+++ React/src/navigation/Sidenav.js
@@ -1,13 +1,13 @@
-import React from "react";
-import "./Sidenav.css";
-import HomeIcon from "@mui/icons-material/Home";
-import SearchIcon from "@mui/icons-material/Search";
-import ExploreIcon from "@mui/icons-material/Explore";
-import SlideshowIcon from "@mui/icons-material/Slideshow";
-import ChatIcon from "@mui/icons-material/Chat";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
-import MenuIcon from "@mui/icons-material/Menu";
+import React from 'react';
+import './Sidenav.css';
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import ExploreIcon from '@mui/icons-material/Explore';
+import SlideshowIcon from '@mui/icons-material/Slideshow';
+import ChatIcon from '@mui/icons-material/Chat';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import MenuIcon from '@mui/icons-material/Menu';
 /*import { Menu } from "@mui/material";*/
 
 function Sidenav() {
@@ -36,7 +36,7 @@ function Sidenav() {
 				</button>
 
 				<button className="sidenav__button">
-					<SlideshowIcon />g
+					<SlideshowIcon />
 					<span>Reels</span>
 				</button>
 
